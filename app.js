8471031d3c140a0ab067176fe6ffe35a5c33b041
Your Name <you@example.com>
2025-05-26 15:57:13 +0800

    A08 & Profile Changes

---
+++ app.js
@@ -0,0 +1,42 @@
+let nextBtn = document.getElementById('next');
+let prevBtn = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list');
+let thumbnailDom = document.querySelector('.carousel .thumbnail');
+
+
+let runTimeOut;
+let timeRunning = 1000;
+
+
+nextBtn.onclick = function () {
+    showsSlider('next');
+};
+
+prevBtn.onclick = function () {
+    showsSlider('prev');
+};
+
+
+function showsSlider(type) {
+    let itemSlider = document.querySelectorAll('.carousel .list .item');
+    let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
+
+    if (type === 'next') {
+        listItemDom.appendChild(itemSlider[0]);
+        thumbnailDom.appendChild(itemThumbnail[0]);
+        carouselDom.classList.add('next');
+    } else {
+        let positionLastItem = itemSlider.length - 1;
+        listItemDom.prepend(itemSlider[positionLastItem]);
+        thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+        carouselDom.classList.add('prev');
+    }
+
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
+}
+
